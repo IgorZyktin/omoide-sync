b32bbe797c218fd0ccdb9b701c0a599de8238c3b
Igor Zyktin <nicord@yandex.ru>
2024-12-06 19:49:04 +0300

    feat: refactored config

--- omoide_sync/cfg.py
+++ omoide_sync/cfg.py
@@ -1,17 +1,15 @@
 """Global configuration."""
 
+import sys
 import tomllib
 from dataclasses import dataclass
 from pathlib import Path
 
-from omoide_sync import const
 
-
-@dataclass
+@dataclass(frozen=True)
 class RawUser:
     """Initial user representation."""
 
-    name: str
     login: str
     password: str
 
@@ -20,48 +18,51 @@ class RawUser:
         return f'RawUser<{self.login}>'
 
 
-@dataclass
+@dataclass(frozen=True)
 class Config:
     """Global configuration."""
 
     api_url: str
-    root_path: Path
-    trash_path: Path
+    source_path: Path
+    archive_path: Path
     log_path: Path
-    supported_formats: set[str]
+    setup_filename: str
+    supported_formats: frozenset[str]
     spacer: str
     dry_run: bool
     users: list[RawUser]
+    raw_setup: dict[str, str]
 
 
 def get_config(config_file: str, dry_run: bool) -> Config:
     """Return Config instance."""
     config_path = Path(config_file)
+
     if not config_path.exists():
         msg = f'Config file does not exist: {config_path.absolute()}'
-        raise RuntimeError(msg)
+        sys.exit(msg)
 
     with open(config_path, 'rb') as config_file:
         config_data = tomllib.load(config_file)
 
     if 'config' not in config_data:
         msg = f"There is no 'config' section in the config file: {config_path.absolute()}"
-        raise RuntimeError(msg)
+        sys.exit(msg)
 
-    root_path = Path(config_data['config']['root_folder'])
-    if not root_path.exists():
-        msg = f'Root folder does not exist: {root_path.absolute()}'
-        raise RuntimeError(msg)
+    source_path = Path(config_data['config']['source_folder'])
+    if not source_path.exists():
+        msg = f'Source folder does not exist: {source_path.absolute()}'
+        sys.exit(msg)
 
-    trash_path = Path(config_data['config']['trash_folder'])
-    if not trash_path.exists():
-        msg = f'Trash folder does not exist: {trash_path.absolute()}'
-        raise RuntimeError(msg)
+    archive_path = Path(config_data['config']['archive_folder'])
+    if not archive_path.exists():
+        msg = f'Archive folder does not exist: {archive_path.absolute()}'
+        sys.exit(msg)
 
     cred_path = Path(config_data['config']['cred_file'])
     if not cred_path.exists():
         msg = f'Credentials file does not exist: {cred_path.absolute()}'
-        raise RuntimeError(msg)
+        sys.exit(msg)
 
     with open(cred_path, 'rb') as cred_file:
         cred_data = tomllib.load(cred_file)
@@ -69,8 +70,8 @@ def get_config(config_file: str, dry_run: bool) -> Config:
     raw_users = cred_data.get('users')
 
     if not raw_users:
-        msg = f'No users are mentioned in credentials file {cred_path.absolute()}'
-        raise RuntimeError(msg)
+        msg = f'No users in credentials file: {cred_path.absolute()}'
+        sys.exit(msg)
 
     users: list[RawUser] = []
     for user_data in cred_data['users']:
@@ -79,25 +80,33 @@ def get_config(config_file: str, dry_run: bool) -> Config:
     log_path = Path(config_data['config']['log_file'])
     if not log_path.parent.exists():
         msg = f'Logging folder does not exist: {log_path.parent.absolute()}'
-        raise RuntimeError(msg)
+        sys.exit(msg)
 
     if not (api_url := config_data['config'].get('api_url')):
         msg = f'No API URL is mentioned in the config file: {config_path.absolute()}'
-        raise RuntimeError(msg)
+        sys.exit(msg)
+
+    if not (setup_filename := config_data['config'].get('setup_filename')):
+        msg = f'No setup filename is mentioned in the config file: {config_path.absolute()}'
+        sys.exit(msg)
+
+    if not (spacer := config_data['config'].get('spacer')):
+        msg = f'No spacer symbol is mentioned in the config file: {config_path.absolute()}'
+        sys.exit(msg)
 
     if not (supported_formats := config_data['config'].get('supported_formats')):
         msg = f'No supported formats are mentioned in the config file: {config_path.absolute()}'
-        raise RuntimeError(msg)
-
-    spacer = config_data['config'].get('spacer', const.DEFAULT_SPACER)
+        sys.exit(msg)
 
     return Config(
         api_url=api_url,
-        root_path=root_path,
-        trash_path=trash_path,
-        dry_run=dry_run,
+        source_path=source_path,
+        archive_path=archive_path,
+        setup_filename=setup_filename,
         log_path=log_path,
+        dry_run=dry_run,
         users=users,
         spacer=spacer,
-        supported_formats=set(supported_formats),
+        supported_formats=frozenset(supported_formats),
+        raw_setup=config_data.get('root', {}),
     )
