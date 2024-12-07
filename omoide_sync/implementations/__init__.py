58b9f099ac4e4dac95d08fc775a181b5207feca2
Igor Zyktin <nicord@yandex.ru>
2024-12-07 18:32:43 +0300

    feat: dropped dead code

--- omoide_sync/implementations/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Namespace for implementations."""
-
-from omoide_sync.implementations.client import SeleniumClient  # noqa: F401
-from omoide_sync.implementations.logic import Logic  # noqa: F401
-from omoide_sync.implementations.storage import FileStorage  # noqa: F401
--- omoide_sync/interfaces/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Namespace for interfaces."""
-
-from omoide_sync.interfaces.abs_client import AbsClient  # noqa: F401
-from omoide_sync.interfaces.abs_logic import AbsLogic  # noqa: F401
-from omoide_sync.interfaces.abs_storage import AbsStorage  # noqa: F401
